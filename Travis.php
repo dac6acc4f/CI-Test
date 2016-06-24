14476b4eda1ade4e0fcfd8b00a7147bad428005b
Jacob Tian <dac6acc4f@users.noreply.github.com>
2016-06-24 19:13:50 +0800

    Update Travis.php
    Hmm.

--- Travis.php
+++ Travis.php
@@ -23,11 +23,11 @@ if(count(glob("plugins/DevTools/ClearSky*.phar")) === 0){
 }else{
     $buildID = "";
 	echo "Server phar created!\n";
-    echo "Uploading to GitHub";
+    echo "Uploading to GitHub</p>";
 	exec( "git config --global user.email 'dac6acc4f@gmail.com'");
 	exec(  "git config --global user.name 'dac6acc4f'");
     echo exec("mkdir travis_builds"); //in case you don't want to have a messy repo ;-)
-    echo exec("git add plugins/DevTools/ClearSky*.phar");
+    echo exec("git add -f plugins/DevTools/ClearSky*.phar");
     echo exec("git commit -m 'Travis CI auto build'". $buildID); //you may want to add the current version here
     echo exec("git push origin php7"); //or some other repos/branches
 	exit(0);
