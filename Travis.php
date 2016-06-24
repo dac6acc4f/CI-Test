e1acef06f40f5ec753abfcd98c48147074f37f45
dac6acc4f <dac6acc4f@gmail.com>
2016-06-24 18:51:50 +0800

    Enable Travis CI to Github repo
    This script is now supposed to automatically push successful  builds
    from Travis CI to the Github repo (where the Travis CI was initiated
    from).

--- Travis.php
+++ Travis.php
@@ -21,6 +21,12 @@ if(count(glob("plugins/DevTools/ClearSky*.phar")) === 0){
 	echo "No server phar created!\n";
 	exit(1);
 }else{
+    $buildID = "";
 	echo "Server phar created!\n";
+    echo "Uploading to GitHub";
+    echo exec("mkdir travis_builds"); //in case you don't want to have a messy repo ;-)
+    echo exec("git add plugins/DevTools/ClearSky*.phar");
+    echo exec("git commit -m 'Travis CI auto build'". $buildID); //you may want to add the current version here
+    echo exec("git push php7"); //or some other branches
 	exit(0);
 }
